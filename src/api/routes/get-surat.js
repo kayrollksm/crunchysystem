586c84a6432267d9dd3713d9eb4dc1c56fba1b40
Khairul Solehen Bin Mokhtar <kayrollksm@gmail.com>
2025-07-18 23:47:10 +0800

    fix: use correct env key in get-surat.js

--- src/api/routes/get-surat.js
+++ src/api/routes/get-surat.js
@@ -3,8 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
+
 
 export default async function handler(req, res) {
   const { id } = req.query
